ce9388e3719b10969581d3867ae5a0b6c7b1af22
Carlos Rojas <carlosrb206@gmail.com>
2021-10-17 13:58:31 -0400

    inquire module

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const generatePage = require('./src/page-template.js');
 
-const [name1, github] = profileDataArgs;
+// const pageHTML = generatePage(name1, github);
 
-
-fs.writeFile('index.html', generatePage(name1, github), err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
